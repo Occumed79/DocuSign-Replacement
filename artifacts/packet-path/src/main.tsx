772d21eec82703c456bbe516ef4659ef89b2a635
Occumed79 <Occumed79@outlook.com>
2026-05-11 23:10:50 -0700

    Enable Tahoe green theme overrides

--- artifacts/packet-path/src/main.tsx
+++ artifacts/packet-path/src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./tahoe-green.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <div className="luminous-orb one" />
+    <div className="luminous-orb two" />
+    <div className="luminous-orb three" />
+    <App />
+  </>
+);
