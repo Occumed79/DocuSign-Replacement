4faa2ceb72ea3461df3d2c8dff9948565bbb8acd
Occumed79 <Occumed79@outlook.com>
2026-05-21 03:48:50 -0700

    fix: add missing dist declaration for branding

---
+++ lib/db/dist/schema/branding.d.ts
@@ -0,0 +1,3 @@
+import { PgTableWithColumns } from 'drizzle-orm/pg-core';
+export declare const clinicBrandingTable: PgTableWithColumns<any>;
+export type ClinicBranding = typeof clinicBrandingTable.$inferSelect;
