3b2376b4f7c608f7e1878e834475b090726d6f43
Occumed79 <Occumed79@outlook.com>
2026-05-21 03:48:53 -0700

    fix: add missing dist declaration for webauthn

---
+++ lib/db/dist/schema/webauthn.d.ts
@@ -0,0 +1,3 @@
+import { PgTableWithColumns } from 'drizzle-orm/pg-core';
+export declare const webauthnCredentialsTable: PgTableWithColumns<any>;
+export declare const webauthnChallengesTable: PgTableWithColumns<any>;
